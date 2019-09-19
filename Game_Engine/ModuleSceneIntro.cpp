04945ef5e066cbff1bdf1511d1cd7e0516a0bbee
vsRushy <vsRushy@users.noreply.github.com>
2019-09-19 20:17:08 +0200

    Added ImGui tests

--- Game_Engine/ModuleSceneIntro.cpp
+++ Game_Engine/ModuleSceneIntro.cpp
@@ -1,3 +1,7 @@
+#include "ImGui/imgui.h"
+#include "ImGui/imgui_impl_opengl2.h"
+#include "ImGui/imgui_impl_sdl.h"
+
 #include "Globals.h"
 #include "Application.h"
 #include "ModuleSceneIntro.h"
@@ -17,6 +21,14 @@ bool ModuleSceneIntro::Start()
 	LOG("Loading Intro assets");
 	bool ret = true;
 
+	/* Load ImGui */
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	ImGui::StyleColorsDark();
+	ImGui_ImplSDL2_InitForOpenGL(App->window->window, App->renderer3D->context);
+	ImGui_ImplOpenGL2_Init();
+
 	App->camera->Move(vec3(1.0f, 1.0f, 0.0f));
 	App->camera->LookAt(vec3(0, 0, 0));
 
@@ -28,15 +40,43 @@ bool ModuleSceneIntro::CleanUp()
 {
 	LOG("Unloading Intro scene");
 
+	ImGui_ImplOpenGL2_Shutdown();
+	ImGui_ImplSDL2_Shutdown();
+	ImGui::DestroyContext();
+
 	return true;
 }
 
 // Update
 update_status ModuleSceneIntro::Update(float dt)
 {
+	ImGui_ImplOpenGL2_NewFrame();
+	ImGui_ImplSDL2_NewFrame(App->window->window);
+	ImGui::NewFrame();
+
+	{
+		bool show_demo_window = true;
+		ImGui::ShowDemoWindow(&show_demo_window);
+	}
+
+	{
+		ImGui::Begin("Sample window");
+		if (ImGui::Button("Close"))
+		{
+			return UPDATE_STOP;
+		}
+		ImGui::Text("Lorem Ipsum");
+		ImGui::End();
+	}
+
+
 	Plane p(0, 1, 0, 0);
 	p.axis = true;
 	p.Render();
+
+	/* Render ImGui */
+	ImGui::Render();
+	ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
 
 	return UPDATE_CONTINUE;
 }

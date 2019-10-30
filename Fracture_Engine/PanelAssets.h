f0caf6efe2bb7d6bccfd269c2b66d6393e10334a
vsRushy <vsRushy@users.noreply.github.com>
2019-10-30 16:56:51 +0100

    Assets panel ready

---
+++ Fracture_Engine/PanelAssets.h
@@ -0,0 +1,15 @@
+#ifndef __PANEL_ASSETS_H__
+#define __PANEL_ASSETS_H__
+
+#include "Panel.h"
+
+class PanelAssets : public Panel
+{
+public:
+	PanelAssets(std::string name, bool active = true);
+	~PanelAssets();
+
+	bool Update() override;
+};
+
+#endif /* __PANEL_ASSETS_H__ */

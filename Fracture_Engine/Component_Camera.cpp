7554462c0c0554635a1b2e3d6f4928d89fd4c442
martitorras <marti.torras98@gmail.com>
2019-11-11 13:52:46 +0100

    camera

--- Fracture_Engine/Component_Camera.cpp
+++ Fracture_Engine/Component_Camera.cpp
@@ -5,6 +5,14 @@
 ComponentCamera::ComponentCamera(GameObject* target) : Component(target)
 {
 	type = COMPONENT_TYPE::CAMERA;
+	frustum.type = math::PerspectiveFrustum;
+	frustum.front = math::float3::unitZ;
+	frustum.up = math::float3::unitY;
+	frustum.pos = math::float3::zero;
+	frustum.nearPlaneDistance = 1.0f;
+	frustum.farPlaneDistance = 100.0f;
+	frustum.verticalFov = DEGTORAD * 60;
+	frustum.horizontalFov = 2.0f * atanf(aspect_ratio * tanf(frustum.verticalFov * 0.5f));
 }
 
 ComponentCamera::~ComponentCamera()
@@ -12,3 +20,7 @@ ComponentCamera::~ComponentCamera()
 	
 }
 
+float ComponentCamera::GetCameraFOV() const {
+
+	return RADTODEG * frustum.verticalFov;
+}
--- Fracture_Engine/Component_Camera.h
+++ Fracture_Engine/Component_Camera.h
@@ -5,6 +5,8 @@
 #include <list>
 
 #include "Component.h"
+#include "MathGeoLib/include/Geometry/Frustum.h"
+
 
 class ComponentCamera : public Component
 {
@@ -12,6 +14,29 @@ public:
 	ComponentCamera(GameObject* target);
 	~ComponentCamera();
 
+	void SetCameraFOV(const float& fov);
+	void SetCameraNearPlane(const float& near_plane);
+	void SetCameraFarPlane(const float& far_plane);
+
+	float GetCameraFOV() const;
+	float GetCameraNearPlane() const;
+	float GetCameraFarPlane() const;
+
+	float fov = 0.0f;
+	float min_fov = 0.0f;
+	float max_fov = 0.0f;
+
+	float near_plane = 0.0f;
+	float min_near_plane = 0.0f;
+	float max_near_plane = 0.0f;
+
+	float far_plane = 0.0f;
+	float min_far_plane = 0.0f;
+	float max_far_plane = 0.0f;
+
+private:
+	Frustum frustum;
+	float aspect_ratio = 1.7778f;
 };
 
 #endif /* __COMPONENT_CAMERA_H__ */

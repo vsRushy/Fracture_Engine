33adb675fbf087223ea6f94ca2bf0600613003f1
martitorras <marti.torras98@gmail.com>
2019-11-13 23:09:51 +0100

    more camera

--- Fracture_Engine/Component_Camera.cpp
+++ Fracture_Engine/Component_Camera.cpp
@@ -6,6 +6,15 @@
 ComponentCamera::ComponentCamera(GameObject* target) : Component(target)
 {
 	type = COMPONENT_TYPE::CAMERA;
+	frustum.type = PerspectiveFrustum;
+	frustum.front = float3(0, 0, 1);
+	frustum.up = float3(0, 1, 0);
+	frustum.pos = float3(0, 0, 0);
+	frustum.nearPlaneDistance = 1.0f;
+	frustum.farPlaneDistance = 500.0f;
+	frustum.verticalFov = DEGTORAD * 120.0f;
+	frustum.horizontalFov = 0;
+	aspect_ratio = 1.5f;
 }
 
 ComponentCamera::~ComponentCamera()
@@ -16,4 +25,60 @@ ComponentCamera::~ComponentCamera()
 bool ComponentCamera::Update(float dt)
 {
 	return true;
+}
+
+void ComponentCamera::SetCameraFOV(const float& fov)
+{
+	frustum.verticalFov = DEGTORAD * fov;
+	frustum.horizontalFov = 2.0f * atanf(aspect_ratio * tanf(frustum.verticalFov * 0.5f));
+}
+
+void ComponentCamera::SetCameraNearPlane(const float& near_plane)
+{
+	frustum.nearPlaneDistance = near_plane;
+}
+
+void ComponentCamera::SetCameraFarPlane(const float& far_plane)
+{
+	frustum.farPlaneDistance = far_plane;
+}
+
+void ComponentCamera::SetAspectRatio(const float& a_r)
+{
+	aspect_ratio = a_r;
+}
+
+void ComponentCamera::SetCameraSpeed(const float& sp)
+{
+	speed = sp;
+}
+
+float ComponentCamera::GetCameraVerticalFOV() const
+{
+	return RADTODEG * frustum.verticalFov;
+}
+
+float ComponentCamera::GetCameraHorizontalFOV() const
+{
+	return RADTODEG * frustum.horizontalFov;
+}
+
+float ComponentCamera::GetCameraNearPlane() const
+{
+	return frustum.nearPlaneDistance;
+}
+
+float ComponentCamera::GetCameraFarPlane() const
+{
+	return frustum.farPlaneDistance;
+}
+
+float ComponentCamera::GetAspectRatio() const
+{
+	return aspect_ratio;
+}
+
+float ComponentCamera::GetCameraSpeed() const
+{
+	return speed;
 }
--- Fracture_Engine/Component_Camera.h
+++ Fracture_Engine/Component_Camera.h
@@ -2,6 +2,8 @@
 #define __COMPONENT_CAMERA_H__
 
 #include "Component.h"
+#include "MathGeoLib\include\Geometry\Frustum.h"
+#include "MathGeoLib\include\Geometry\AABB.h"
 
 class ComponentCamera : public Component
 {
@@ -10,6 +12,28 @@ public:
 	~ComponentCamera();
 
 	bool Update(float dt);
+
+	void SetCameraFOV(const float& fov);
+	void SetCameraNearPlane(const float& near_plane);
+	void SetCameraFarPlane(const float& far_plane);
+	void SetAspectRatio(const float& a_r);
+	void SetCameraSpeed(const float& sp);
+
+	float GetCameraVerticalFOV() const;
+	float GetCameraHorizontalFOV() const;
+	float GetCameraNearPlane() const;
+	float GetCameraFarPlane() const;
+	float GetAspectRatio() const;
+	float GetCameraSpeed() const;
+
+private:
+	//void CalculateViewMatrix();
+
+private:
+	Frustum frustum;
+	float aspect_ratio = 0.0f;
+	mat4x4 ViewMatrix, ViewMatrixInverse;
+	float speed = 0.0f;
 };
 
 #endif /* __COMPONENT_CAMERA_H__ */

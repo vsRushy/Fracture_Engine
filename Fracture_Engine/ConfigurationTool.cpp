9c88608d62c5cfa8f093ad79472d1b6475d73765
vsRushy <vsRushy@users.noreply.github.com>
2019-11-15 00:44:02 +0100

    Creating JSON wrapper

---
+++ Fracture_Engine/ConfigurationTool.cpp
@@ -0,0 +1,71 @@
+#include "ConfigurationTool.h"
+
+ConfigurationTool::ConfigurationTool(const char* file, const char* node)
+{
+	j_value = json_parse_file(file);
+
+	if (j_value == NULL)
+	{
+		LOG(LOG_INFORMATION, "Couldn't find the JSON file %s. A new file will be created instead.", file);
+		j_value = json_value_init_object();
+	}
+
+	j_object = json_value_get_object(j_value);
+	j_node_object = json_object_get_object(j_object, node);
+}
+
+ConfigurationTool::~ConfigurationTool()
+{
+	json_value_free(j_value);
+}
+
+int ConfigurationTool::GetInt(const char* name)
+{
+	return (int)json_object_dotget_number(j_node_object, name);
+}
+
+uint ConfigurationTool::GetUint(const char* name)
+{
+	return (uint)json_object_dotget_number(j_node_object, name);
+}
+
+float ConfigurationTool::GetFloat(const char* name)
+{
+	return (float)json_object_dotget_number(j_node_object, name);
+}
+
+bool ConfigurationTool::GetBool(const char* name)
+{
+	return (bool)json_object_dotget_boolean(j_node_object, name);
+}
+
+const char* ConfigurationTool::GetString(const char* name)
+{
+	return json_object_dotget_string(j_node_object, name);
+
+}
+
+void ConfigurationTool::SetInt(const char* name, const int& value)
+{
+	json_object_set_number(j_node_object, name, (double)value);
+}
+
+void ConfigurationTool::SetUint(const char* name, const uint& value)
+{
+	json_object_set_number(j_node_object, name, (double)value);
+}
+
+void ConfigurationTool::SetFloat(const char* name, const float& value)
+{
+	json_object_set_number(j_node_object, name, (double)value);
+}
+
+void ConfigurationTool::SetBool(const char* name, const bool& value)
+{
+	json_object_set_boolean(j_node_object, name, value);
+}
+
+void ConfigurationTool::SetString(const char* name, const char* value)
+{
+	json_object_set_string(j_node_object, name, value);
+}
---
+++ Fracture_Engine/ConfigurationTool.h
@@ -0,0 +1,34 @@
+#ifndef __CONFIGURATION_TOOL_H__
+#define __CONFIGURATION_TOOL_H__
+
+#include "JSON/parson.h"
+
+#include "Globals.h"
+
+/* JSON custom wrapper */
+class ConfigurationTool
+{
+public:
+	ConfigurationTool(const char* file, const char* node);
+	~ConfigurationTool();
+
+public:
+	int GetInt(const char* name);
+	uint GetUint(const char* name);
+	float GetFloat(const char* name);
+	bool GetBool(const char* name);
+	const char* GetString(const char* name);
+
+	void SetInt(const char* name, const int& value);
+	void SetUint(const char* name, const uint& value);
+	void SetFloat(const char* name, const float& value);
+	void SetBool(const char* name, const bool& value);
+	void SetString(const char* name, const char* value);
+
+public:
+	JSON_Value* j_value = nullptr;
+	JSON_Object* j_object = nullptr;
+	JSON_Object* j_node_object = nullptr;
+};
+
+#endif /* __CONFIGURATION_TOOL_H__ */

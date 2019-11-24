f28b2cd6373b85fdcc334c35fe64999e4d296ea7
vsRushy <vsRushy@users.noreply.github.com>
2019-11-24 23:22:56 +0100

    Doing cam

--- Fracture_Engine/ComponentCamera.cpp
+++ Fracture_Engine/ComponentCamera.cpp
@@ -1,8 +1,22 @@
 #include "ComponentCamera.h"
 
+#include "GL/glew.h"
+
 ComponentCamera::ComponentCamera(GameObject* target) : Component(target)
 {
 	type = COMPONENT_TYPE::CAMERA;
+
+	frustum.pos = float3::zero;
+	frustum.up = float3::unitY;
+	frustum.front = float3::unitZ;
+
+	aspect_ratio = 1.7f; /* 16/9ar */
+
+	vertical_fov = frustum.verticalFov = 60.0f * DEGTORAD;
+	horizontal_fov = frustum.horizontalFov = 2.0f * atanf(tanf(frustum.verticalFov / 2.0f) * aspect_ratio);
+
+	near_plane = frustum.nearPlaneDistance = 0.1f;
+	far_plane = frustum.farPlaneDistance = 1000.0f;
 }
 
 ComponentCamera::~ComponentCamera()
@@ -38,4 +52,34 @@ void ComponentCamera::Load(JSON_Object* json_object)
 void ComponentCamera::Save(JSON_Array* json_array) const
 {
 
+}
+
+Frustum ComponentCamera::GetFrustrum() const
+{
+	return frustum;
+}
+
+float4x4 ComponentCamera::GetViewMatrix() const
+{
+	float4x4 fvm = frustum.ViewMatrix();
+	return fvm.Transposed();
+}
+
+float4x4 ComponentCamera::GetProjectionMatrix() const
+{
+	return frustum.ProjectionMatrix();
+}
+
+void ComponentCamera::SetPosition(const float3& position)
+{
+	frustum.pos = position;
+}
+
+void ComponentCamera::SetFrustumVertices()
+{
+	float3 vertices[8] = { float3::zero };
+	frustum.GetCornerPoints(vertices);
+
+	glBindBuffer(GL_ARRAY_BUFFER, id_f_vertices);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 8 * 3, vertices, GL_DYNAMIC_DRAW);
 }
--- Fracture_Engine/ComponentCamera.h
+++ Fracture_Engine/ComponentCamera.h
@@ -1,9 +1,11 @@
 #ifndef __COMPONENT_CAMERA_H__
 #define __COMPONENT_CAMERA_H__
 
-#include "MathGeoLib/include/Geometry/Frustum.h"
+#include "MathGeoLib.h"
 
 #include "Component.h"
+
+#include "Globals.h"
 
 class ComponentCamera : public Component
 {
@@ -20,16 +22,31 @@ public:
 	void Load(JSON_Object* json_object) override;
 	void Save(JSON_Array* json_array) const override;
 
+public:
+	/* Getters */
+	Frustum  GetFrustrum() const;
+
+	float4x4 GetViewMatrix() const;
+	float4x4 GetProjectionMatrix() const;
+
+	/* Setters */
+	void SetPosition(const float3& position);
+
+	void SetFrustumVertices();
+
 private:
 	Frustum frustum;
-	
-	float3 cam_points;
+
+	float aspect_ratio = 1.0f;
 
 	float horizontal_fov = NULL;
 	float vertical_fov = NULL;
-
+	
 	float far_plane = NULL;
 	float near_plane = NULL;
+
+private:
+	uint id_f_vertices = -1;
 };
 
 #endif /* __COMPONENT_CAMERA_H__ */
